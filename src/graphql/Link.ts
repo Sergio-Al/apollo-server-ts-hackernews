54e727ead81ef42a8e77e6ae5d6a1f581dd97840
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2022-01-25 22:07:19 -0400

    Implementing Queries
    I've implemented the following:
    - I've created a new folder that care of our schemas (graphql folder)
    - We can create our schemas inside the graphql folder
    - I've created a link Type via the nexus syntax
    - If you run via npm run dev our schema.graphql and nexus-typegen.ts will be updated

---
+++ src/graphql/Link.ts
@@ -0,0 +1,37 @@
+import { extendType, objectType } from "nexus";
+import { NexusGenObjects } from "../../nexus-typegen";
+
+export const Link = objectType({
+  name: "Link",
+  definition(t) {
+    t.nonNull.int("id");
+    t.nonNull.string("description");
+    t.nonNull.string("url");
+  },
+});
+
+// dummy data
+let links: NexusGenObjects["Link"][] = [
+  {
+    id: 1,
+    url: "www.google.com",
+    description: "main page of google",
+  },
+  {
+    id: 2,
+    url: "graphql.org",
+    description: "GraphQL official website",
+  },
+];
+
+export const LinkQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.nonNull.field("feed", {
+      type: "Link",
+      resolve(parent, args, context, info) {
+        return links;
+      },
+    });
+  },
+});
--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,11 @@
-import { makeSchema } from "nexus"
-import { join } from "path"
+import { makeSchema } from "nexus";
+import { join } from "path";
+import * as types from "./graphql";
 
 export const schema = makeSchema({
-  types: [],
+  types,
   outputs: {
     schema: join(__dirname, "..", "schema.graphql"),
-    typegen: join(__dirname, "..", "nexus-typegen.ts")
+    typegen: join(__dirname, "..", "nexus-typegen.ts"),
   },
-})
+});
