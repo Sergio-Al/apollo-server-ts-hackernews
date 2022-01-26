530959d45242d9f2e055bee8a7feadeaf42b5e3e
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2022-01-26 00:32:15 -0400

    Adding mutations
    At first glance mutations with nexus seems confusing, but if you understand, you realize that this is the best way to make a mutation, with TS this mutations will be type-safe avoid weird problems in the future

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from "nexus";
+import { arg, extendType, idArg, nonNull, objectType, stringArg } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
@@ -31,6 +31,79 @@ export const LinkQuery = extendType({
       type: "Link",
       resolve(parent, args, context, info) {
         return links;
+      },
+    });
+    t.field("link", {
+      type: "Link",
+      args: {
+        id: nonNull(idArg()),
+      },
+      resolve(parent, args, context) {
+        return links.filter((link) => link.id.toString() === args.id)[0];
+      },
+    });
+  },
+});
+
+export const LinkMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("post", {
+      type: "Link",
+      args: {
+        description: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+      },
+
+      resolve(parent, args, context) {
+        const { description, url } = args;
+
+        let idCount = links.length + 1;
+        const link = {
+          id: idCount,
+          description,
+          url,
+        };
+        links.push(link);
+        return link;
+      },
+    });
+    t.nonNull.field("updateLink", {
+      type: "Link",
+      args: {
+        id: nonNull(idArg()),
+        url: stringArg(),
+        description: stringArg(),
+      },
+      resolve(parent, args, context) {
+        const { id, url, description } = args;
+
+        links = links.map((link) =>
+          link.id.toString() === id
+            ? {
+                id: Number(id),
+                description: description || link.description,
+                url: url || link.url,
+              }
+            : link
+        );
+
+        return links.filter((link) => link.id.toString() === id)[0];
+      },
+    });
+    t.nonNull.field("deleteLink", {
+      type: "Link",
+      args: {
+        id: nonNull(idArg()),
+      },
+      resolve(parent, args, context) {
+        const removedLink = links.filter(
+          (link) => link.id.toString() === args.id
+        )[0];
+
+        links = links.filter((link) => link.id.toString() !== args.id);
+
+        return removedLink;
       },
     });
   },
